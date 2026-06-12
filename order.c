e1c6f4dfe472b0b70103f7da17be91776f4f330c
memonqaid-coder <memonqaid@gmail.com>
2026-06-12 09:53:37 +0000

    12-jun

---
+++ order.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int order_id=123;
+    float price=65.43;
+    int quantity=100;
+    printf("\norder id is :%d",order_id);
+    printf("\nprice :%f",price);
+    printf("\nquantity :%d",quantity);
+    return 0;
+
+}
