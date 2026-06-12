dbcf7c839dad589a79ea19bea3aec57a4ae9677e
memonqaid-coder <memonqaid@gmail.com>
2026-06-12 03:15:55 -0700

    employee.c

---
+++ employee.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+  int emp-id=223334;
+long salary=3344557799;
+int bonus=100;
+printf("\nemployee id is:%d",emp-id);
+printf("\nsalary is:%ld",salary);
+printf("\nbonus:%d",bonus);
+printf("\ntotal:%ld",salary+bonus);
+return 0;
+}
