ab9f6cc42d4bb780ac3d91f7de73309cfeaff82c
memonqaid-coder <memonqaid@gmail.com>
2026-06-13 10:50:40 +0000

    13 jun

--- employee.c
+++ employee.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 int main(){
+<<<<<<< HEAD
   int emp-id=223334;
 long salary=3344557799;
 int bonus=100;
@@ -9,3 +10,13 @@ printf("\nbonus:%d",bonus);
 printf("\ntotal:%ld",salary+bonus);
 return 0;
 }
+=======
+  int emp_id=223334;
+long salary=3344557799;
+int bonus=100;
+printf("\nemployee id is:%d",emp_id);
+printf("\nsalary is:%ld",salary);
+printf("\nbonus:%d",bonus);
+return 0;
+}
+>>>>>>> 6d004a7 (13 jun)
---
+++ inputoutput.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+  int marks;
+  printf("enter marks:");
+  scanf("%d",&marks);
+  printf("marks is:%d",marks);
+  return 0;
+}
