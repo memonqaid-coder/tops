53cd54be37a1e5b991209510b2074aef114b7071
memonqaid-coder <memonqaid@gmail.com>
2026-06-17 13:13:44 +0000

    1

--- module 2/task4.c
+++ module 2/task4.c
@@ -1,5 +1,9 @@
-#include<stdio.h>
+/*Q:Compile your hello.c file using the GCC command line (gcc hello.c -o hello) and run the
+   output file to display the message in your terminal.<br><br><em><strong>Hint:</strong> On Windows, the output file will be hello.exe; on Linux/Mac, it will be hello.</em> 
+A:*/
+   #include<stdio.h>
 int main(){
    printf("hello,Instagram World\n");
     return 0;
 }
+/*gcc code: cd module\ 2/ --> ENTER -->gcc task4.c -o task -->ENTER --> ./task -->ENTER */
