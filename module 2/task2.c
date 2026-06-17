54283819954db95d0d0d8ec760ac9556588946af
memonqaid-coder <memonqaid@gmail.com>
2026-06-17 12:39:23 +0000

    2

---
+++ module 2/task2.c
@@ -0,0 +1,7 @@
+ /*Q: open VS Code or DevC++ and create a new file named hello.c. Write a C program that prints 'Hello, Instagram World!' to the console.
+   A: */
+#include<stdio.h>
+int main(){
+   printf("hello,Instagram World");
+    return 0;
+}
