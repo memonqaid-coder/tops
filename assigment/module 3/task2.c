38315a6a9598ebd3b54f132cd579c6970303e0b4
memonqaid-coder <memonqaid@gmail.com>
2026-06-18 12:53:53 +0000

    1

---
+++ assigment/module 3/task2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+int CUSTOMER_ID=2344990088;
+ float GST_Rate = 0.28f;
+ float itemprice=220.00f;
+ float gstAmount=itemprice *gstAmount;
+ float finalprice =itemprice+ gstAmount;
+ printf("___ ZOMATO ORDER BILL ___\n");
+ printf("CUSTOMER_ID:");
+ printf("------------------\n");
+ printf("item price:Rs. %.2f\n,item price");
+ printf("GST (28%):Rs. %.2f\n,gst rate");
+ printf("----------\n");
+ printf("GRAND TOTAL:Rs. %2f\n,total amount");
+ printf("-----THANK YOU-----\n");
+    return 0;
+}
