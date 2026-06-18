0400adc915a3c2ac21fd7fce0239e168750a8be1
memonqaid-coder <memonqaid@gmail.com>
2026-06-18 11:57:09 +0000

    11

---
+++ assigment/module 3/task1.c
@@ -0,0 +1,19 @@
+/* Declare variables for a Flipkart product: productName (as a string), 
+  price (float), and rating (double). Assign sample values and print each 
+  variable with its data type.*/
+#include <stdio.h>
+int main() {
+    // 1. Variable declaration and initialization
+    char productName[] = "Apple Watch";
+    float price = 4999.99f;                  
+    double rating = 4.8;                      
+    // 2. Printing variables along with their data types
+    printf(" __Flipkart Product Details__ \n");
+    printf("Product Name : %s\n", productName);
+    /*printf("Data Type    : String (char[])\n");*/
+    printf("Price        : %.2f\n", price);
+    /*printf("Data Type    : Float\n");*/
+    printf("Rating       : %.1lf\n", rating);
+    /*printf("Data Type    : Double\n");*/
+    return 0;
+}
