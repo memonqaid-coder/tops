24e70952aebd9d75cd3ecdf0f093b483dc5bb0b2
memonqaid-coder <memonqaid@gmail.com>
2026-06-19 12:06:34 +0000

    7

--- conditionalOperator.c
+++ conditionalOperator.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
 /*<,>,<=,>=,==,!=*/
 int main(){
-    int age=45;
-    if (age>=42){
+    int age=48;
+    /*int member=3;*/ 
+    /*if (member<3);
+    if (member>3);*/
+    if(age>=42)
+    {
         printf("Not eligible for rafting");
-    }
-    int member=3;
-    if (member<3){
-        printf("Not allowed");
-    }
-    if (member>3){
-        printf("Arrange other members_Required");
+       /* printf("\nNot allowed",member=4);
+        printf("\nArrange other members_Required",member=2);*/
     }
     return 0;
 }
