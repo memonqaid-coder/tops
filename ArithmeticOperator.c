21062e22b136b92be8e64f0fa3ed64b0adc19ff9
memonqaid-coder <memonqaid@gmail.com>
2026-06-19 13:24:05 +0000

    2

---
+++ ArithmeticOperator.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a=2,b=3;
+    printf("\naddition is %d",a+b);
+    printf("\nsubstraction is %d",a-b);
+    printf("\nmultiply is %d",a*b);
+    printf("\ndivide is %d",a/b);
+    printf("\nmodulous is %d",a%b);
+    return 0;
+}
