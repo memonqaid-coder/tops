0093a39781d83f44a0c03ed46aae266cee847b38
memonqaid-coder <memonqaid@gmail.com>
2026-06-20 09:49:00 +0000

    z

---
+++ SALARY INCRIMENT.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int current_salary=30000;
+    int appraisal_amount=5555;
+    int TOTALAMOUNT=current_salary+appraisal_amount;
+    /*printf("\ncurrent_salary %d",current_salary);
+    printf("\nBONUS %d",appraisal_amount);*/
+    printf("\nTOTALAMOUNT =%d",TOTALAMOUNT);
+    return 0;
+}
