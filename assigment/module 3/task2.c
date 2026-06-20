129a49926d3c3ef0d1ab27f9d6aa8bdf51887dd9
memonqaid-coder <memonqaid@gmail.com>
2026-06-20 09:04:23 +0000

    q

--- assigment/module 3/task2.c
+++ assigment/module 3/task2.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
 int CUSTOMER_ID=2344990088;
- float GST_Rate = 5.0f;
+ float GST_Rate = 18.0f;
  float itemprice=840.10f;
  float gstAmount=(itemprice *GST_Rate)/100;
  float finalprice =itemprice+ gstAmount;
@@ -9,7 +9,7 @@ int CUSTOMER_ID=2344990088;
  printf("\nCUSTOMER_ID:22334477");
  printf("\n------------------");
  printf("\nitem price:Rs. %.2f",itemprice);
- printf("\ngst (5%%):Rs. %.2f", gstAmount);
+ printf("\ngst (18%%):Rs. %.2f", gstAmount);
  printf("\n----------");
  printf("\nGRAND TOTAL:Rs. %.2f",finalprice);
  printf("\n-----THANK YOU-----");
