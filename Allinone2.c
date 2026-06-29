116cc2f5edd7fce77fcf248328b078e6901a8743
memonqaid-coder <memonqaid@gmail.com>
2026-06-29 13:54:39 +0000

    1

--- Allinone2.c
+++ Allinone2.c
@@ -24,10 +24,51 @@ int main(){
     default:
     printf("\nExit");
     }
-    printf("\n__Loop__");
-    {for (int i=1; i<=10;i++)
+    printf("\n__For Loop__");
+    for (int i=1; i<=10;i++)
     {printf("\nRoll No.%d is Present",i);
     }
+    int pin;
+    printf("\n__While Loop__");
+    while (pin!=22334){
+        printf("\nEnter your pin");
+        scanf("%d",&pin);
+    }
+    printf("\nLogin Successful");
+    int select;
+    while(3){
+        printf("\nEnter your choice");
+        scanf("%d",&select);
+    {
+    select,1;
+    printf("\ndeposit");
+        break;
+    select,2;
+    printf("\nwithdraw");
+    break;
+    select,3;
+    printf("\nBalance Check");
+
+    }
+    printf("\nThank you");
+    
+    
+    }
+    printf("\n__Nested Loop__");
+    for (int i=1; i<=3;i++){
+        for (int k=1; k<=6;k++){
+            printf("\nRAW no is %d and column is %d",i,k);
+        }
+        
+    }
+    printf("\n__Break&Continue__");
+    for (int A = 1; A <=10; A++){
+        if (A==4){
+            printf("\nStudent %d is Absent",A);
+            continue;
+        }
+        printf("\nStudent %d is Present",A);      
     }
     return 0;
-}
+    }
+    
